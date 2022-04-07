989256e89c78f39dc14503beec3fc5a64e2b4128
yugpsyfer <yugansh.singh@gmail.com>
2022-04-07 17:58:27 +0530

    supcon loss

--- Data/mini_imagenet.py
+++ Data/mini_imagenet.py
@@ -79,12 +79,8 @@ class MiniImageNet(Dataset):
         file_name = files.split("_")[0]
         label = torch.tensor(data=self.label_dict[file_name])
         _embedding_ = torch.tensor(data=self.embeddings[file_name].flatten(), dtype=torch.double)
-        # embedding = torch.cat((_embedding_, _embedding_), dim=0)
-        # embedding = torch.fr
+        embedding = [_embedding_, _embedding_]
         with Image.open(self.rootDir + files) as im:
             data = self.tensorTransformation(im)
 
-        # data = torch.cat((data[0], data[1]), dim=0)
-        print(data[0].shape)
-        9/0
         return (data, label, embedding)  # Return a tuple format (data,label)
